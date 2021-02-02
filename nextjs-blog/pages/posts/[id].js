c9be0f0802bda42d53453be97094943bb659c1c8
Reyomi101 <rhomiple.itd.pps@gmail.com>
2021-02-02 16:27:56 +0800

    made some updates

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,8 +1,11 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -22,11 +25,20 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
       <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+          <br />
+          {postData.id}
+          <br />
+          <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lighText}>
+              <Date dateString={postData.date} /> 
+            </div>
+            <br />
+            <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+          </article>
       </Layout>
     )
   }
