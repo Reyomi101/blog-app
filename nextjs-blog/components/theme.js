02a77a5ee3ebbc341b4e8d76687cb8aec34ee41d
Reyomi101 <rhomiple.itd.pps@gmail.com>
2021-02-08 18:15:13 +0800

    update folders and files

--- nextjs-blog/components/theme.js
+++ nextjs-blog/components/theme.js
@@ -1,17 +1,15 @@
 // Material UI imports
-import { createMuiTheme } from '@material-ui/core'
+import { createMuiTheme } from '@material-ui/core';
 
-  
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#2196f3'
-        }
-    },
-    background: {
-        default: '#fff'
-    }
-
-})
+	palette: {
+		primary: {
+			main: '#2196f3',
+		},
+	},
+	background: {
+		default: '#fff',
+	},
+});
 
 export default theme;
